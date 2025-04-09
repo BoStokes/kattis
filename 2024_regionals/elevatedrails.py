da3828bbd254ccb4932dc15a5ae150bf5ad11a4a
BoStokes <beetlenutria1@gmail.com>
2025-04-09 00:22:41 -0500

    finished rails

--- 2024_regionals/elevatedrails.py
+++ 2024_regionals/elevatedrails.py
@@ -1,4 +1,5 @@
 from collections import *
+from functools import cache
 
 n, q = map(int, input().split())
 
@@ -31,6 +32,7 @@ for station in range(1, n+1):
 
 get_centers = dict()
 get_dist_from_center = dict()
+get_dist_from_edge = dict()
 get_width = dict()
 for island_num, island_graph in islands.items():
     # perform kahn's algorithm to find the center of each island tree
@@ -53,11 +55,32 @@ for island_num, island_graph in islands.items():
         leaves = new_leaves
     get_centers[island_num] = leaves
 
-
-    visited = [False] * (n+1)
-    curr_nodes = leaves
-    count = len(curr_nodes)
-    steps = 0
-    while count < len(island_graph):
-        new_nodes
+    # now perform bfs from the centers and find the furthest distance to a leaf
+    visited = set()
+    width = 0
+    queue = deque(((node, 0) for node in get_centers[island_num]))
+    while queue:
+        node, steps = queue.popleft()
+        if node in visited: continue
+        visited.add(node)
+        for nbr in graph[node]:
+            if nbr not in visited:
+                queue.append((nbr, steps + 1))
+        get_dist_from_center[node] = steps
+        width = max(width, steps)
     
+    get_width[island_num] = 2 * width + len(get_centers[island_num])
+
+@cache
+def get_dist_from_edge(station):
+    island_num = get_island[station]
+    return 1 + get_dist_from_center[station] + get_width[island_num] // 2
+
+for _ in range(q):
+    start_station, end_station = map(int, input().split())
+    start_island, end_island = get_island[start_station], get_island[end_station]
+
+    middle_island = start_island ^ end_island
+
+    total = get_dist_from_edge(start_station) + get_width[middle_island] + get_dist_from_edge(end_station)
+    print(total)
